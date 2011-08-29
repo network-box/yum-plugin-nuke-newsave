59c0718cb59623abc88a845a29bc46eee4531954
Mathieu Bridon <bochecha@fedoraproject.org>
2011-08-29 11:33:42 +0800

    Better setup.py
    Added a URL to the project hosted at Gitorious.
    
    Added a few trove classifiers, to find it more easily at Pypi.
    
    Made the yum requirement more explicit.

--- setup.py
+++ setup.py
@@ -10,15 +10,22 @@ setup(name='yum-plugin-nuke-newsave',
         description='Automatically remove the .rpmnew/.rpmsave files after a yum transaction',
         long_description=README,
         classifiers=[
+            "Intended Audience :: System Administrators",
+            "License :: OSI Approved :: GNU General Public License (GPL)",
+            "Operating System :: POSIX :: Linux",
             "Programming Language :: Python",
+            "Topic :: System :: Installation/Setup",
+            "Topic :: System :: Software Distribution",
+            "Topic :: System :: Systems Administration",
             ],
         author='Mathieu Bridon',
         author_email='bochecha@fedoraproject.org',
-        url='',
+        url='https://gitorious.org/yum-plugin-nuke-newsave/yum-plugin-nuke-newsave',
         license='GPLv3+',
         py_modules=['nuke-newsave'],
         requires = [
-            # The following are not available as distutils modules
-            # 'yum',
+            # The following are not available as distutils modules, but listing
+            # them here seems like the nice thing to do for package maintainers
+            # "yum>=3.2.29", # Not tested with older releases (that means RHEL6)
             ],
         )
